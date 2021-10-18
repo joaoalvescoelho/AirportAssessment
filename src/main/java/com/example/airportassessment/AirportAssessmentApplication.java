2d7071070465151b44bd48e2c33d6a7b79f62135
João Alves Coelho <jmdjac@gmail.com>
2021-10-18 20:59:03 +0100

    Initial commit

---
+++ src/main/java/com/example/airportassessment/AirportAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.airportassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportAssessmentApplication.class, args);
+    }
+
+}
